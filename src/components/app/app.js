93a74ef7a7fd3bc97a9224f9e93506c635bf5572
Anton <sychiov2015@yandex.ru>
2022-04-05 19:05:14 +0500

    #

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,17 @@
-import './app.css';
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployersList from '../employers-list/employers-list';
 import EmployersAddForm from '../employers-add-form/employers-add-form';
 
+import './app.css';
+
 function App() {
+    const data = [
+        {name: 'John C.', salary: 800, key: 1},
+        {name: 'Alex M.', salary: 3000, key: 2},
+        {name: 'Carl W.', salary: 5000, key: 3},
+    ];
     return (
         <div className='app'>
             <AppInfo/>
@@ -15,7 +21,7 @@ function App() {
                 <AppFilter/>
             </div>
 
-            <EmployersList/>
+            <EmployersList data={data}/>
             <EmployersAddForm/>
         </div>
     )
